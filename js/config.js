00c7786b9be467a19401f1294a70d2f6e24a3126
Kaustav Chatterjee <kaustav.chatterjee@gmail.com>
2020-04-12 17:02:15 +0000

    Adding initial version of files

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'eu-west-2_1mle8FA8n', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '19c5ee6qn7igc291nljuj64soj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://d2b9a5f3ze.execute-api.eu-west-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+    }
+};
